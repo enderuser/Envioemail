3303767dcc24d0d05f2890af8cb6c74538a49154
EnderUser <enderuser@outlook.com>
2024-01-03 23:33:20 -0300

    Teste de envio apenas em console teve sucesso

--- envia_email/views.py
+++ envia_email/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.mail import send_mail
 
 def envia_email(request):
+    send_mail('Assunto de teste', 'Este é o corpo do email para teste', 'wender@tisistema.com.br', ['kayky@tisistema.com.br'])
     return HttpResponse('Olá')
